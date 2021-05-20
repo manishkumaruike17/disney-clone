e1ce030cd5220e843702033d62a736b3def7a310
manishkumaruike17 <manishkumardevelops17@gmail.com>
2021-05-20 21:32:39 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Login from "./components/Login";
+import './App.css';
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="App">
+     <Router>
+       <Header />
+       <Switch>
+         <Route exact path="/">
+           <Login />
+          </Route> 
+        </Switch>
+      </Router>  
+    </div>
+  );
+}
+
+export default App;
